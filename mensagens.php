d0b9434f213e798c96a8e2ed40fc4fadf9d0e088
Fábio Garcia <fabio@unipar.br>
2025-12-12 13:43:42 -0300

    Versão Final 1.0.0.3

--- mensagens.php
+++ mensagens.php
@@ -35,28 +35,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <label for="senha">Senha:</label>
         <input type="password" name="senha" placeholder="Digite sua Senha" required>
         <button type="submit">Enviar</button>
-
+        <div class="mensagens">
         <?php if (isset($result) && $result->num_rows >0) : ?>
-        <?php endif; ?>    
-            <h2>Mensagens</h2>
+            <h2 style="text-aling:center">Mensagens</h2>
             <ul>
                 <?php while($row = $result-> fetch_assoc()) : ?>
                     <li>
                         <strong>Nome: </strong> <?php echo $row["nome"]; ?> <br>
                         <strong>E-mail: </strong> <?php echo $row["email"]; ?> <br>
                         <strong>Mensagem: </strong> <?php echo $row["mensagem"]; ?> <br>  
-                        <strong>Data e Hora: </strong> <?php echo $row["data"]. " às ".$row["data"]; ?><br>
+                        <strong>Data e Hora: </strong> <?php echo $row["data"]. " às ".$row["hora"]; ?><br><br>
                     </li> 
                 <?php endwhile; ?>                               
             </ul>
-
+        <?php else : ?>
+            <p>Nenhuma mensagem.</p>   
+        <?php endif; ?>             
         <!-- Mensagem aparece exatamente aqui -->
         <?php if (!empty($mensagemErro)) : ?>
             <p style="color:red; margin-top:10px;">
                 <?= $mensagemErro ?>
             </p>
         <?php endif; ?>
-
+        </div>            
     </form>
 </body>
 
